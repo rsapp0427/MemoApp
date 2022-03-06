394283ee48a999ed07ff43fa60dacb9cc149a3d8
Ryousuke <100859574+rsapp0427@users.noreply.github.com>
2022-03-07 02:35:28 +0900

    Create Hello component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Hello } from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>Unko</Hello>
+      <Hello bang style={{ fontSize: 16 }}>Small Unko</Hello>
+      <Hello bang>Unko</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
---
+++ src/components/Hello.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string, bool, shape } from 'prop-types';
+
+export const Hello = (props) => {
+	const { children, bang, style } = props;
+	return(
+		<View>
+			<Text style={[styles.text, style]}>
+				{`Hello ${children}${bang ? '!' : ''}`}
+			</Text>
+		</View>
+	);
+}
+
+Hello.propTypes = {
+	children: string.isRequired,
+	bang: bool,
+	style: shape(),
+};
+
+Hello.defaultProps = {
+	bang: false,
+	style: null,
+};
+
+const styles = StyleSheet.create({
+  text:{
+		color: 'white',
+		backgroundColor: 'blue',
+		fontSize: 40,
+		fontWeight: 'bold',
+		padding: 16,
+  },
+});
